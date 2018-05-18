74bedd14f85747fce89aff9530ecf91415995f2a
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-18 00:13:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+  if array.size == 1 
+    return array.join
+  elsif array.size == 2 
+    return array.join(" and ")
+  else
+    last_word = array.last
+    new_array = array.pop
+    new_array.push(" and #{last_word}")
+    return new_array.join(", ")
+  end
 
 end
