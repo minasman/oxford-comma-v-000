584bfcd83a686aa2d15b9f9bfb3ab17742dcdec7
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-18 00:21:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+  if array.size == 1 
+    return array.join
+  elsif array.size == 2 
+    return array.join(" and ")
+  else
+    last_word = array.last
+    array.pop
+    array.push("and #{last_word}")
+    return array.join(", ")
+  end
 
 end
